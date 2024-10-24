166f03376ac302ea8b3e184cdac2d59e4b9ac5dc
FrankFU323 <tianrui.fu@mail.utoronto.ca>
2024-10-24 16:45:44 -0400

    Update 03-clean_data.R
    Remove the columns with NA we don't need

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -18,6 +18,29 @@ data <- read_csv("/Users/frankstrove/Desktop/STA304/US Preseident forecast/data/
   clean_names()
 
 just_trump_high_quality <- data |>
+  select(
+    -"sponsor_ids" , 
+    -"sponsors" , 
+    -"sponsor_candidate_id", 
+    -"sponsor_candidate", 
+    -"sponsor_candidate_party", 
+    -"endorsed_candidate_id", 
+    -"endorsed_candidate_name", 
+    -"endorsed_candidate_party", 
+    -"subpopulation", 
+    -"tracking", 
+    -"notes", 
+    -"url_topline", 
+    -"source", 
+    -"internal", 
+    -"partisan", 
+    -"seat_name", 
+    -"ranked_choice_round",
+    -"url_article",
+    -"url_crosstab"
+  )
+
+just_trump_high_quality <- just_trump_high_quality |>
   filter(
     candidate_name == "Donald Trump",
     numeric_grade >= 2.7 # Need to investigate this choice - come back and fix. 
@@ -27,10 +50,12 @@ just_trump_high_quality <- data |>
     state = if_else(is.na(state), "National", state), # Hacky fix for national polls - come back and check
     end_date = mdy(end_date)
   ) |>
-  filter(end_date >= as.Date("2024-07-21")) |> # When Trump declared
+  filter(end_date >= as.Date("2024-07-21")) |># When Trump declared
+  drop_na(pct, sample_size) |>
   mutate(
     num_Trump = round((pct / 100) * sample_size, 0) # Need number not percent for some models
   )
+
 
 #### Plot data ####
 base_plot <- ggplot(just_trump_high_quality, aes(x = end_date, y = pct)) +
@@ -61,3 +86,4 @@ base_plot +
 
 #### Save data ####
 write_parquet(x = just_trump_high_quality, sink = "data/02-analysis_data/analysis_data.parquet")
+
