1a9fcd36c20e0e587922adbb5c4db9d2d9dad235
FrankFU323 <tianrui.fu@mail.utoronto.ca>
2024-10-24 17:00:26 -0400

    Update 06-model_data.R
    change the model

--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -17,41 +17,43 @@ just_trump_high_quality <- read_parquet("data/02-analysis_data/analysis_data.par
 
 ### Model data ####
 # Model 1: pct as a function of end_date
-model_date <- lm(pct ~ end_date, data = just_trump_high_quality)
-
+model_all <- lm(pct ~ pollster + numeric_grade + pollscore + methodology + 
+                   transparency_score + state + end_date + sample_size, data = just_trump_high_quality)
+summary(model_all)
 # Model 2: pct as a function of end_date and pollster
-model_date_pollster <- lm(pct ~ end_date + pollster, data = just_trump_high_quality)
+model_chosen <- lm(pct ~ end_date + state + sample_size, data = just_trump_high_quality)
+summary(model_chosen)
 
 # Augment data with model predictions
+library(modelr)
+
 just_trump_high_quality <- just_trump_high_quality |>
-  mutate(
-    fitted_date = predict(model_date),
-    fitted_date_pollster = predict(model_date_pollster)
-  )
+  add_predictions(model_all, var = "fitted_all") |>
+  add_predictions(model_chosen, var = "fitted_chosen")
 
 # Plot model predictions
 # Model 1
 ggplot(just_trump_high_quality, aes(x = end_date)) +
   geom_point(aes(y = pct), color = "black") +
-  geom_line(aes(y = fitted_date), color = "red", linetype = "dotted") +
+  geom_line(aes(y = fitted_all), color = "red", linetype = "dotted") +
   theme_classic() +
   labs(y = "Harris percent", x = "Date", title = "Linear Model: pct ~ end_date")
 
 # Model 2
 ggplot(just_trump_high_quality, aes(x = end_date)) +
   geom_point(aes(y = pct), color = "black") +
-  geom_line(aes(y = fitted_date_pollster), color = "red", linetype = "dotted") +
+  geom_line(aes(y = fitted_chosen), color = "red", linetype = "dotted") +
   facet_wrap(vars(pollster)) +
   theme_classic() +
   labs(y = "Harris percent", x = "Date", title = "Linear Model: pct ~ end_date + pollster")
 
 #### Save model ####
 saveRDS(
-  model_date,
-  file = "models/model_date.rds"
+  model_all,
+  file = "models/model_all.rds"
 )
 
 saveRDS(
-  model_date_pollster,
-  file = "models/model_date_pollster.rds"
+  model_chosen,
+  file = "models/model_chosen.rds"
 )
